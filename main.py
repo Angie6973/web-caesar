b44fd1925a932cf5af8576e96174fcedec62027a
Angelika Stephanie Pierce <angiekinter@gmail.com>
2018-03-31 16:06:04 -0500

    finish web-caeser

--- main.py
+++ main.py
@@ -9,31 +9,31 @@ encrypt_form="""
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <!-- create your form here -->
       
        
-            <form action="/encrypt" method="post">
+            <form action="/" method="post">
                  <label>
                     Rotate by: 
                     <input type="text" name="rot" value="0"/>
                  </label>
-                 <textarea name="text" ></textarea>
+                 <textarea name="text" >{0}</textarea>
                  <input type="submit" value="Submit Query"/>
             </form>
             
@@ -42,28 +42,22 @@ encrypt_form="""
     </body>
 </html>
 """
-    text = request.form["text"]
-    rot=request.form["rot"]
-    encrypted = caesar.rotate_string(text, int(rot))
+    
 
 @app.route("/")
 def index():
    
 
-    return encrypt
+    return encrypt_form.format("")
 
+@app.route("/", methods=['POST'])
+def encrypt():
+    text = request.form["text"]
+    rot=request.form["rot"]
+    encrypted = rotate_string(text, int(rot))
+    return encrypt_form.format(encrypted)
 app.run()
 
-encrypt="""
-<!DOCTYPE html>
-    <head>
-    </head>
-    <body> 
-        <h1>{0} </h1>
-    </body>
-
-<html>
 
 
-"""
-encrypt=encrypt.format(encrypted)
+
